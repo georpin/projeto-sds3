55299ed7bafd96b810d0c795864c31783d31c99c
Danilo George <danilo.george@hotmail.com>
2021-05-05 11:54:08 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://www.linkedin.com/in/danilo-george-pinheiro-mba-cpre-fl-psm-01299382/" target="_blank" rel="noreferrer">Danilo George Pinheiro</a></p>
+                <p className="text-light"><small><strong>Desevolvido com Spring e React</strong></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
+
+
+
+
+
